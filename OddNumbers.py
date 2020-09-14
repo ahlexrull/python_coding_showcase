1948730cd49b4e0cf6335269f59250a7b4dfdf14
Alexandria Rull <ahlexrull@gmail.com>
2020-09-14 09:01:15 +0000

    Added OddNumbers and SumEven modules, altered EvenNumbers and Odd Numbers to include array output

--- OddNumbers.py
+++ OddNumbers.py
@@ -1,18 +1,18 @@
-# Write a C program to print all even numbers between 1 to 100. - using while loop
+# Write a C program to print all odd number between 1 to 100.
 
-def even_num(num):
-  i = 1
+def odd_num(num):
+  i = 0
   while i <= num:
-    if i % 2 == 0 :
+    if i % 2 != 0:
       print(i)
     i += 1
 
 #this can be used in general and then a loop just print out the returned arr so you dont need the function above
 def even_num_arr(num):
   arr = []
-  i = 0
+  i = 1
   while i <= num:
-    if i != 0 :
+    if i % 2 != 0:
       arr.append(i)
     i += 2
   return arr
--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-"""
-Task List 
-- Write a C program to print all even numbers between 1 to 100. - using while loop
-- Write a C program to print all odd number between 1 to 100.
-- Write a C program to find sum of all natural numbers between 1 to n.
-- Write a C program to find sum of all even numbers between 1 to n.
-- Write a C program to find sum of all odd numbers between 1 to n.
-- Write a C program to print multiplication table of any number.
-- Write a C program to count number of digits in a number.
-- Write a C program to find first and last digit of a number.
-"""
 
 from EvenNumbers import *
+from OddNumbers import *
+from SumEven import *
 
-even_num(10)
+# even_num_arr(100) => returns array
+# even_num(100)
+
+# odd_nums_arr(100) => returns array
+# odd_nums(100)
+
+print(sum_even(100))
